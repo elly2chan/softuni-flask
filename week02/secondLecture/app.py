a8b3f813697d9a4feafd6d63cae0c246c6bc1ff3
elly2chan <elena.iv.konstantinova@gmail.com>
2024-10-26 17:11:54 +0300

    fix

--- week02/secondLecture/app.py
+++ week02/secondLecture/app.py
@@ -9,7 +9,7 @@ from werkzeug.exceptions import NotFound
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    f'postgresql://postgres:terminator123@localhost:5432/store'
+    f'postgresql://postgres:test@localhost:5432/store'
 
 
 class Base(DeclarativeBase):
--- week03/thirdLecture/app.py
+++ week03/thirdLecture/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from decouple import config
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run()
+db_user = config('DB_USER')
+db_password = config('DB_PASSWORD')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    f'postgresql://{db_user}:{db_password}@localhost:5432/store'
